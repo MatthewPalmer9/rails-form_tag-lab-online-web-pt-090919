87c2359553ccb011a0e7336641cb475cff413c5c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-02 03:17:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     @student.save
-    redirect_to student_path(@student)
+    redirect_to "/students"
   end
 
 end
